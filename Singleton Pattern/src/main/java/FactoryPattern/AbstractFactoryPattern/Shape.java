6636ea82756003ecd14e3cbb5998b29eeee81791
Hty <15161656368@163.com>
2025-08-12 14:56:50 +0800

    提交文件

---
+++ Singleton Pattern/src/main/java/FactoryPattern/AbstractFactoryPattern/Shape.java
@@ -0,0 +1,5 @@
+package FactoryPattern.AbstractFactoryPattern;
+
+public interface Shape {
+    void draw();
+}
---
+++ Singleton Pattern/src/main/java/FactoryPattern/IteratorPattern/Container.java
@@ -0,0 +1,5 @@
+package FactoryPattern.IteratorPattern;
+
+public interface Container {
+    public Iterator getIterator();
+}
